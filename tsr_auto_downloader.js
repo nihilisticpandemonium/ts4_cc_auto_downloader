256ec6ec748d165feca1190e4919f27bd23c6d11
NihilisticPandemonium <152417+nihilisticpandemonium@users.noreply.github.com>
2018-12-27 05:33:29 -0800

    Update category downloaders to wait for their first date to arrive, silence page errors, and add a blessed console UI to watch progress.

--- tsr_auto_downloader.js
+++ tsr_auto_downloader.js
@@ -8,8 +8,10 @@ const cheerio = require('cheerio')
 const fs = require('fs')
 const levelup = require('levelup')
 const leveldown = require('leveldown')
+const blessed = require('blessed');
 
 const time_format = "YYYY-MM-DD"
+const first_date = moment("2014-09-06", time_format);
 
 const categories = ["clothing", "shoes", "hair", "makeup", "accessories", "eyecolors", "skintones", "walls", "floors", "objects", "objectrecolors", "lots", "sims", "pets"]
 const category_type = {
@@ -24,9 +26,9 @@ const category_type = {
     "floors": "BB",
     "objects": "BB",
     "objectrecolors": "BB",
-    "lots": "Tray",
-    "sims": "Tray",
-    "pets": "Tray"
+    "lots": "Tray/Lots",
+    "sims": "Tray/Sims",
+    "pets": "Tray/Pets"
 }
 
 const cldsdb = levelup(leveldown('data/category_date.db'));
@@ -42,6 +44,165 @@ if (!String.prototype.replaceLast) {
 
         return this.toString();
     };
+}
+
+var screen = blessed.screen({
+    smartCSR: true
+});
+
+screen.title = 'TSR Auto Downloader';
+
+var openPhantomInstances = {};
+
+screen.key(['escape', 'q', 'C-c'], (ch, key) => {
+    Object.keys(openPhantomInstances).forEach((k) => { openPhantomInstances[k].exit(); });
+    return process.exit(0);
+});
+
+var displayForm = blessed.form({
+    top: 'left',
+    left: 'left',
+    width: '100%',
+    height: '100%',
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0'
+        },
+    }
+});
+
+screen.append(displayForm);
+var categoryDates = blessed.list({
+    parent: displayForm,
+    top: '15%',
+    left: '10%',
+    width: '35%',
+    height: '70%',
+    interactive: 'false',
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'black',
+        bg: 'red',
+        border: {
+            fg: '#d0d0d0'
+        },
+        item: {
+            fg: 'white',
+            bg: 'red',
+        },
+        selected: {
+            fg: 'white',
+            bg: 'red',
+        }
+    },
+});
+
+screen.append(categoryDates);
+
+var categoryDateLabel = blessed.text({
+    parent: displayForm,
+    top: '5%',
+    left: '10%',
+    width: '35%',
+    height: '10%',
+})
+
+categoryDateLabel.setText("Category Dates");
+
+screen.append(categoryDateLabel);
+
+var currentlyDownloading = blessed.list({
+    parent: displayForm,
+    top: '15%',
+    left: '50%',
+    width: '35%',
+    height: '70%',
+    interactive: 'false',
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'black',
+        bg: 'cyan',
+        border: {
+            fg: '#d0d0d0'
+        },
+        item: {
+            fg: 'white',
+            bg: 'cyan',
+        },
+        selected: {
+            fg: 'white',
+            bg: 'cyan'
+        }
+    }
+})
+
+screen.append(currentlyDownloading);
+
+var currentlyDownloadingLabel = blessed.text({
+    parent: displayForm,
+    top: '5%',
+    left: '50%',
+    width: '35%',
+    height: '10%',
+})
+
+currentlyDownloadingLabel.setText('Currently Downloading');
+
+screen.append(currentlyDownloadingLabel);
+
+var currentlyDownloadingTbl = {};
+
+var currentlyExecutingText = blessed.text({
+    top: '90%', 
+    left: '5%',
+    width: '90%',
+    height: '10%',
+    aligh: 'left'
+})
+
+screen.append(currentlyExecutingText);
+
+screen.render();
+
+function log(text) {
+    currentlyExecutingText.setText(text);
+    screen.render();
+}
+
+var category_downloaders = [];
+
+function updateCategoryDates() {
+    categoryDates.clearItems();
+    const renderCategoryDate = (category) => {
+        if (typeof category.date !== 'undefined') {
+            categoryDates.addItem(category.category + ": " + category.date.format(time_format) + " (P: " + category.page + ")");
+        }
+    }
+
+    category_downloaders.forEach(renderCategoryDate);
+    screen.render();
+}
+
+function updateCurrentlyDownloadingList() {
+    currentlyDownloading.clearItems();
+    const renderCurrentlyDownloading = (key) => {
+        var text = key + " (" + currentlyDownloadingTbl[key].category + ")";
+        if (typeof currentlyDownloadingTbl[key].fileName !== 'undefined') {
+            text += ' => ' + currentlyDownloadingTbl[key].fileName;
+        }
+        currentlyDownloading.addItem(text);
+    }
+    Object.keys(currentlyDownloadingTbl).forEach(renderCurrentlyDownloading);
+    screen.render();
 }
 
 /* Basic flow:
@@ -99,15 +260,13 @@ const category_downloader = class {
         if (incDate) {
             this.date.add(1, 'd')
             this.page = 1
-            console.log("New date for " + this.category + ": " + this.date.format(time_format));
             cldsdb.put(this.category, this.date.format(time_format));
         } else {
             this.page = this.page + 1;
-            console.log("New page for " + this.category + ": " + this.page);
         }
+        updateCategoryDates();
     }
     dl_children($, children) {
-        console.log('dl_children');
         var childPromises = [];
         children.each((child) => {
             var data_href = children[child].attribs['data-href'];
@@ -126,10 +285,13 @@ const category_downloader = class {
                     var _ph, _page;
                     phantom.create().then((ph) => {
                         _ph = ph;
+                        openPhantomInstances[this.category] = ph;
                         return ph.createPage();
                     }).then((page) => {
                         _page = page;
-                        page.on('onError', function() { return ;});
+                        page.on('onError', function() {
+                            return;
+                        });
                         return _page.open(url);
                     }).then((status) => {
                         _page.property('content').then((content) => {
@@ -141,13 +303,16 @@ const category_downloader = class {
                                 var page_close_promise = _page.close();
                                 page_close_promise.then(() => {
                                     _ph.exit();
+                                    delete openPhantomInstances[this.category];
                                 })
                                 var now = moment();
                                 if (detailChildren.length > 0) {
                                     Promise.all(this.dl_children($, detailChildren));
                                 }
                                 this.next_page(detailChildren.length <= 21);
-                                this.download().then(() => { resolve(true); });
+                                this.download().then(() => {
+                                    resolve(true);
+                                });
                             });
                         });
                     });
@@ -159,6 +324,21 @@ const category_downloader = class {
 
 var phantom_instance_pool = [];
 
+function addCurrentlyDownloading(itemID, category) {
+    currentlyDownloadingTbl[itemID] = { category: category };
+    updateCurrentlyDownloadingList();
+}
+
+function deleteCurrentlyDownloading(itemID) {
+    delete currentlyDownloadingTbl[itemID];
+    updateCurrentlyDownloadingList();
+}
+
+function setCurrentlyDownloadingFileName(itemID, fileName) {
+    currentlyDownloadingTbl[itemID].fileName = fileName;
+    updateCurrentlyDownloadingList();
+}
+
 const detail_downloader = class {
     constructor(category, attr) {
         this.itemID = attr.match(".+/id/([0-9]+)/")[1];
@@ -167,80 +347,109 @@ const detail_downloader = class {
         this.downloaded = false;
     }
     download() {
-        dldb.get(this.itemID).then((value) => {
-            return;
-        }).catch((err) => {
-            console.log("Downloading " + this.itemID + "...");
+        var _waitingForDelete = false;
+        if (this.category == "Lots" || this.category === "Sims" || this.category === "Pets") {
+            _waitingForDelete = true;
+            dldb.del(this.itemID).then(() => {
+                _waitingForDelete = false;
+            }).catch((err) => {
+                _waitingForDelete = false;
+            });
+        }
+        const waitForDelete = () => {
             return new Promise(resolve => {
-                var _ph, _page;
-                phantom.create().then((ph) => {
-                    _ph = ph;
-                    return ph.createPage();
-                }).then((page) => {
-                    _page = page;
-                    page.on('onError', function() { return ;});
-                    return _page.open(this.url);
-                }).then((status) => {
-                    const waitForDownload = () => {
-                        return new Promise(resolve => {
-                            const delay = 500;
-                            const f = () => {
-                                if (this.downloaded === true) {
-                                    resolve(true);
-                                } else {
-                                    setTimeout(f, delay);
-                                }
-                            };
-                            f();
-                        });
+                const delay = 500;
+                const f = () => {
+                    if (!_waitingForDelete) {
+                        resolve(true);
+                    } else {
+                        setTimeout(f, delay);
                     }
-
-                    _page.property('content').then((content) => {
-                        var url = null;
-                        _page.on('onConsoleMessage', function(msg) {
-                            if (msg.match('http://d27wosp86lso6u.cloudfront.net/downloads') !== null) {
-                                url = msg;
-                            }
+                }
+                f();
+            });
+        }
+        waitForDelete().then((value) => {
+            dldb.get(this.itemID).then((value) => {
+                return new Promise(resolve => { resolve(true); });
+            }).catch((err) => {
+                return new Promise(resolve => {
+                    var _ph, _page;
+                    phantom.create().then((ph) => {
+                        _ph = ph;
+                        openPhantomInstances[this.itemID] = ph;
+                        return ph.createPage();
+                    }).then((page) => {
+                        _page = page;
+                        page.on('onError', function() {
+                            return;
                         });
-                        _page.evaluate(function(itemID) {
-                            var urlOut = null;
-                            _dl(itemID, null, function(url, data) {
-                                console.log(url);
-                            });
-                        }, this.itemID);
-                        const get_url = () => {
+                        return _page.open(this.url);
+                    }).then((status) => {
+                        addCurrentlyDownloading(this.itemID, this.category);
+                        const waitForDownload = () => {
                             return new Promise(resolve => {
                                 const delay = 500;
                                 const f = () => {
-                                    if (url !== null) {
-                                        resolve(url);
+                                    if (this.downloaded === true) {
+                                        resolve(true);
                                     } else {
                                         setTimeout(f, delay);
                                     }
-                                }
+                                };
                                 f();
                             });
                         }
-                        get_url().then((url) => {
-                            function grabFilenameFromResponse(headers) {
-                                return headers.match("filename=\"(.+)\"")[1];
-                            }
-                            axios.get(url).then((response) => {
-                                var file_name = grabFilenameFromResponse(response.headers['content-disposition']).replace(/[^a-z0-9]/gi, '_').toLowerCase().replaceLast("_package", ".package").replaceLast("_zip", ".zip");
-                                var path = "/home/whiro/s4s/" + category_type[this.category] + '/';
-                                if (!fs.existsSync(path + file_name)) {
-                                    console.log("Saving download to " + path + file_name);
-                                    fs.writeFileSync(path + file_name, response.data);
-                                    dldb.put(this.itemID, true);
+
+                        _page.property('content').then((content) => {
+                            var url = null;
+                            _page.on('onConsoleMessage', function(msg) {
+                                if (msg.match('http://d27wosp86lso6u.cloudfront.net/downloads') !== null) {
+                                    url = msg;
                                 }
-                                this.downloaded = true;
                             });
-                        });
-                        waitForDownload().then(() => {
-                            var page_close_promise = _page.close();
-                            page_close_promise.then(() => {
-                                _ph.exit();
-                                resolve(true);
+                            _page.evaluate(function(itemID) {
+                                var urlOut = null;
+                                _dl(itemID, null, function(url, data) {
+                                    console.log(url);
+                                });
+                            }, this.itemID);
+                            const get_url = () => {
+                                return new Promise(resolve => {
+                                    const delay = 500;
+                                    const f = () => {
+                                        if (url !== null) {
+                                            resolve(url);
+                                        } else {
+                                            setTimeout(f, delay);
+                                        }
+                                    }
+                                    f();
+                                });
+                            }
+                            get_url().then((url) => {
+                                function grabFilenameFromResponse(headers) {
+                                    return headers.match("filename=\"(.+)\"")[1];
+                                }
+                                axios.get(url).then((response) => {
+                                    var file_name = grabFilenameFromResponse(response.headers['content-disposition']).replace(/[^a-z0-9]/gi, '_').toLowerCase().replaceLast("_package", ".package").replaceLast("_zip", ".zip");
+                                    var path = "/home/whiro/s4s/" + category_type[this.category] + '/';
+                                    if (!fs.existsSync(path + file_name)) {
+                                        setCurrentlyDownloadingFileName(this.itemID, file_name);
+                                        fs.writeFileSync(path + file_name, response.data);
+                                        dldb.put(this.itemID, true);
+                                    }
+                                    this.downloaded = true;
+                                });
+                            });
+                            waitForDownload().then(() => {
+                                deleteCurrentlyDownloading(this.itemID);
+                                var page_close_promise = _page.close();
+                                page_close_promise.then(() => {
+                                    _ph.exit();
+                                    delete openPhantomInstances[this.itemID];
+                                    resolve(true);
+                                });
                             });
                         });
                     });
@@ -251,30 +460,14 @@ const detail_downloader = class {
 }
 
 var c_dl = [];
-var category_downloaders = [];
+
 categories.forEach((category) => {
-    console.log("Opening " + category + " for downloading.")
+    log("Opening " + category + " for downloading.")
     var c = new category_downloader(category);
     category_downloaders.push(c);
     c_dl.push(c.download());
 });
 
-function printDatesAtInterval() {
-    return new Promise(resolve => {
-        function printCategoryCurrentDates() {
-            const printDate = (category) => {
-                console.log(category.category + ": " + category.date.format(time_format));
-            }
-            console.log("Current category dates:");
-            category_downloaders.forEach(printDate);
-        }
-
-        setInterval(printCategoryCurrentDates, 15000);
-    });
-}
-
-printDatesAtInterval();
-
 Promise.all(c_dl).then(() => {
-    console.log("Finished downloading everything.");
+    log("Finished downloading everything.");
 });
